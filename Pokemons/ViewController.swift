cb87e417abe414f3a982cd473b941f2a48ef1d70
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-11-23 15:34:39 +0530

    Code Refactor. Minor fix

--- Pokemons/ViewController.swift
+++ Pokemons/ViewController.swift
@@ -16,7 +16,7 @@ struct PokemonDetails {
 
 let pokemonsRegistered: [String:PokemonDetails] = [
     "Eevee-card": PokemonDetails(identifier: "Eevee", model: "art.scnassets/Eevee/Eevee.scn"),
-    "Oddish-card":PokemonDetails(identifier: "Oddish", model: "art.scnassets/Oddish/Oddish.scn")
+    "Oddish-card":PokemonDetails(identifier: "Oodish", model: "art.scnassets/Oddish/Oddish.scn")
 ]
 
 class ViewController: UIViewController, ARSCNViewDelegate {
@@ -62,34 +62,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor {
-            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
- 
-            let planeNode = SCNNode(geometry: plane)
-            planeNode.eulerAngles.x = -.pi/2
+            
+            let planeNode = createPlaneOverCard(withImageAnchor: imageAnchor)
             
             node.addChildNode(planeNode)
             
-            guard let pokemonName = imageAnchor.referenceImage.name else {
-                fatalError("Pokemon not registered")
-            }
-            
-            guard let pokemonDetails = pokemonsRegistered[pokemonName] else {
-                fatalError("Pokemon details not registered")
-            }
-            
-            let pockemonScene = SCNScene(named: pokemonDetails.model)
-            
-            if let pockemonNode = pockemonScene?.rootNode.childNode(withName: pokemonDetails.identifier, recursively: true) {
-                pockemonNode.eulerAngles.x = .pi/2
-                //TODO: Scale is not working for pokemon
-//                pockemonNode.position = SCNVector3(0, 0, -0.99)
-//                pockemonNode.transform = SCNMatrix4MakeRotation(-.pi/2, 0, 0, 1)
-               
-                planeNode.addChildNode(pockemonNode)
+            if let pokemonNode = createPokemon3DModel(withImageAnchor: imageAnchor) {
+                planeNode.addChildNode(pokemonNode)
             }
         }
         
         return node
+    }
+    
+    //MARK: - Plane Methods
+    
+    func createPlaneOverCard(withImageAnchor imageAnchor: ARImageAnchor) -> SCNNode{
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi/2
+        
+        return planeNode
+    }
+    
+    //MARK: - Pokemon methods
+    
+    func createPokemon3DModel(withImageAnchor imageAnchor: ARImageAnchor) -> SCNNode? {
+        guard let pokemonName = imageAnchor.referenceImage.name else {
+            fatalError("Pokemon not registered")
+        }
+        
+        guard let pokemonDetails = pokemonsRegistered[pokemonName] else {
+            fatalError("Pokemon details not registered")
+        }
+        
+        let pokemonScene = SCNScene(named: pokemonDetails.model)
+        
+        if let pokemonNode = pokemonScene?.rootNode.childNode(withName: pokemonDetails.identifier, recursively: true) {
+            pokemonNode.eulerAngles.x = .pi/2
+            //TODO: Scale is not working for pokemon
+//                pockemonNode.position = SCNVector3(0, 0, -0.99)
+//                pockemonNode.transform = SCNMatrix4MakeRotation(-.pi/2, 0, 0, 1)
+            
+            return pokemonNode
+        }
+        
+        return nil
     }
 }
